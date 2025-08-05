d6de8f49d2c6258d2da03c556fd6c978fa602827
unknown <dante.h@hotmail.es>
2025-08-05 14:52:53 -0400

    Personalización con HTML y CSS9

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/productos')
+@app.route('/')
 def productos():
     return render_template("productos.html")
 

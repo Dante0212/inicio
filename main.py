363ff31214d1ae5be71c90f5eee60868c6239587
unknown <dante.h@hotmail.es>
2025-08-06 11:46:00 -0400

    cambios html11

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from flask_mail import Mail, Message
 import os
 
 app = Flask(__name__)
-app.secret_key = 'hhfq kcmu efrr cyek'
+
 
 # Configura tu correo
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'cashamaquinola@gmail.com'  # Reemplaza con tu correo
-app.config['MAIL_PASSWORD'] = 'hhfq kcmu efrr cyek'  # Reemplaza con tu contraseña de aplicación
+app.config['MAIL_PASSWORD'] = 'frxz bkrb dfws oaix'  # Reemplaza con tu contraseña de aplicación
 
 mail = Mail(app)
 
@@ -30,7 +30,7 @@ def enviar():
 
     msg = Message("Nueva solicitud de cotización",
                   sender=email,
-                  recipients=['TU_CORREO@gmail.com'])  # Destinatario final
+                  recipients=['cashamaquinola@gmail.com'])  # Destinatario final
     msg.body = f"Nombre: {nombre}\nCorreo: {email}\nMensaje: {mensaje}"
     mail.send(msg)
     flash("Tu mensaje fue enviado correctamente.")
